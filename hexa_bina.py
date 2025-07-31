fb8e0c6be445df9b24dbf1b325653902dc9a4be6
Sammedh Nale <sammedh8nale@gmail.com>
2025-08-01 00:11:40 +0530

    add hexa_bina.py

---
+++ hexa_bina.py
@@ -0,0 +1,12 @@
+def print_formatted(number):
+    width=len(bin(number)[2:])
+    for i in range(1,number+1):
+        stri=str(i).rjust(width)
+        octal=oct(i)[2:].rjust(width)
+        hexa=hex(i)[2:].upper().rjust(width)
+        bina=bin(i)[2:].rjust(width)
+        print(stri,octal,hexa,bina)
+
+if __name__=="__main__":
+    n=int(input("Enter the number: "))
+    print(print_formatted(n))
